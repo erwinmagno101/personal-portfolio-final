6378d5d1d7d2ab748fd28af57e7e0ae892b59642
Dan Erwin Magno <danerwin111@gmail.com>
2024-08-10 00:27:07 +0800

    Main View

---
+++ src/components/MainView.vue
@@ -0,0 +1,183 @@
+<script setup></script>
+
+<template>
+  <div class="max-w-5xl h-5 mx-auto">
+    <div class="flex justify-around flex-wrap-reverse">
+      <div class="basis-1/2 space-y-10 pl-5 mt-24">
+        <div class="name text-6xl font-bold tracking-widest">
+          DAN ERWIN <br />
+          MAGNO
+        </div>
+        <div class="space-y-2">
+          <div class="text-4xl font-bold">Front End Web Developer</div>
+          <div class="text-xl font-bold">
+            Your partner in developing efficient and interactive websites
+          </div>
+          <div class="flex gap-3 flex-wrap mb-3">
+            <div
+              class="border border-black p-2 text-xs font-bold rounded-lg flex items-center gap-1"
+            >
+              <svg
+                width="18px"
+                height="18px"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#000000"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0" />
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    d="M20.501 6.028V6h-.02A10.28 10.28 0 0 0 4.519 6H4.5v.028a10.262 10.262 0 0 0 0 12.944V19h.02a10.28 10.28 0 0 0 15.962 0h.021v-.028a10.262 10.262 0 0 0 0-12.944zM13 6V3.272A4.533 4.533 0 0 1 15.54 6zm2.935 1a16.827 16.827 0 0 1 .853 5H13V7zM12 3.272V6H9.46A4.533 4.533 0 0 1 12 3.272zM12 7v5H8.212a16.827 16.827 0 0 1 .853-5zm-4.787 5H3.226a9.234 9.234 0 0 1 1.792-5h2.984a17.952 17.952 0 0 0-.79 5zm0 1a17.952 17.952 0 0 0 .789 5H5.018a9.234 9.234 0 0 1-1.792-5zm1 0H12v5H9.065a16.827 16.827 0 0 1-.853-5zM12 19v2.728A4.533 4.533 0 0 1 9.46 19zm1 2.728V19h2.54A4.533 4.533 0 0 1 13 21.728zM13 18v-5h3.788a16.827 16.827 0 0 1-.853 5zm4.787-5h3.987a9.234 9.234 0 0 1-1.792 5h-2.984a17.952 17.952 0 0 0 .79-5zm0-1a17.952 17.952 0 0 0-.789-5h2.984a9.234 9.234 0 0 1 1.792 5zm1.352-6h-2.501a8.524 8.524 0 0 0-1.441-2.398A9.306 9.306 0 0 1 19.139 6zM9.803 3.602A8.524 8.524 0 0 0 8.363 6H5.86a9.306 9.306 0 0 1 3.942-2.398zM5.861 19h2.501a8.524 8.524 0 0 0 1.441 2.398A9.306 9.306 0 0 1 5.861 19zm9.336 2.398A8.524 8.524 0 0 0 16.637 19h2.502a9.306 9.306 0 0 1-3.942 2.398z"
+                  />
+                  <path fill="none" d="M0 0h24v24H0z" />
+                </g>
+              </svg>
+              Web Developer
+            </div>
+            <div
+              class="border border-black p-2 text-xs font-bold rounded-lg flex items-center gap-1"
+            >
+              <svg
+                width="18px"
+                height="18px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0" />
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    d="M17 17L22 12L17 7M7 7L2 12L7 17M14 3L10 21"
+                    stroke="#000000"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </g>
+              </svg>
+              Front End
+            </div>
+            <div
+              class="border border-black p-2 text-xs font-bold rounded-lg flex items-center gap-1"
+            >
+              <svg
+                width="18px"
+                height="18px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z"
+                    stroke="#000000"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path
+                    d="M12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12Z"
+                    stroke="#000000"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </g>
+              </svg>
+              Philippines
+            </div>
+          </div>
+        </div>
+        <button class="border border-black p-5">DOWNLOAD CV</button>
+      </div>
+      <div class="basis-1/2">
+        <div class="container w-fit h-fit mx-auto relative">
+          <div class="inner-container w-full h-full relative">
+            <div class="img-container overflow-hidden">
+              <img class="img" src="/me.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.name::after {
+  content: '';
+  display: block;
+  background-color: black;
+  width: 40px;
+  height: 10px;
+  border-radius: 10px;
+  margin-top: 10px;
+}
+
+.img-container {
+  height: 420px;
+  width: 340px;
+}
+
+.img {
+  height: 470px;
+  width: 340px;
+}
+
+@property --angle {
+  syntax: '<angle>';
+  initial-value: 0deg;
+  inherits: false;
+}
+
+.container::before,
+.container::after {
+  --angle: 0deg;
+  content: '';
+  width: calc(100% + 20px);
+  height: calc(70% + 20px);
+  background-image: conic-gradient(from var(--angle), transparent 10%, black);
+  position: absolute;
+  z-index: -2;
+  top: 65%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  animation: spin 5s linear infinite paused;
+}
+
+.container::before {
+  filter: blur(1.5rem);
+  opacity: 0.5;
+}
+
+.inner-container::before {
+  content: '';
+  width: calc(100% + 10px);
+  height: calc(70% + 10px);
+  background-color: white;
+  border: 2px solid white;
+  position: absolute;
+  z-index: -1;
+  top: 65%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.container:hover::before,
+.container:hover::after {
+  animation-play-state: running;
+}
+
+@keyframes spin {
+  from {
+    --angle: 0deg;
+  }
+  to {
+    --angle: 360deg;
+  }
+}
+</style>
