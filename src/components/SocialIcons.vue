2c537055cfab2df13564ad8d65c7693e8f0c039e
Dan Erwin Magno <danerwin111@gmail.com>
2024-08-10 15:57:41 +0800

    Create SocialIcons.vue

---
+++ src/components/SocialIcons.vue
@@ -0,0 +1,50 @@
+<script setup>
+import CustomIcons from './CustomIcons.vue'
+
+const props = defineProps({
+  iconName: { type: String, required: true },
+  customStyle: { type: String }
+})
+</script>
+<template>
+  <div class="border border-black rounded-full h-fit w-fit parent icon-container overflow-clip">
+    <div class="relative icon w-full h-full p-2">
+      <CustomIcons :name="props.iconName" :customStyle="props.customStyle" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.icon::after {
+  content: '';
+  position: absolute;
+  transform-origin: center;
+  width: 100%;
+  height: 0%;
+  bottom: 0;
+  right: 0;
+  background-color: black;
+  z-index: -1;
+  transition: height 0.3s ease-in-out;
+}
+
+.icon {
+  transition: color 0.3s ease-in-out;
+}
+
+.icon-container {
+  transition: scale 0.2s linear;
+}
+
+.icon-container:hover {
+  scale: 1.2;
+
+  .icon::after {
+    height: 100%;
+  }
+
+  .icon {
+    color: white;
+  }
+}
+</style>
